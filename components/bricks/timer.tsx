94fb1a197163588998a257dfed5279f2fd900370
Haller125 <connect@spektal.com>
2024-03-27 16:06:50 +0500

    add timer component

---
+++ components/bricks/timer.tsx
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+
+let destDate:Date = new Date(2024, 5, 13, 10)
+
+type time = {
+    days:number,
+    hours:number,
+    minutes:number,
+    seconds:number,
+}
+//@ TODO errors with second, client has 6, while server has 5(7)
+const Timer = () => {
+    const calculateTimeRemaining:Function = ():time => {
+        const currentTime = new Date().getTime();
+        const timeDifference = destDate.getTime() - currentTime;
+
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        return { days, hours, minutes, seconds };
+    };
+
+    const [timeRemaining, setTimeRemaining] = useState<time>(calculateTimeRemaining());
+
+    useEffect(() => {
+        const timer:NodeJS.Timeout = setInterval(() => {
+            setTimeRemaining(calculateTimeRemaining());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return (
+        <div className={"py-5 relative w-[650px] grid grid-cols-4 content-between items-center"}>
+            <div className={"text-white flex items-end mr-5"}>
+                <p className={"font-bold text-timer-num uppercase text-center mr-[5px]"}>{timeRemaining.days}</p>
+                <p className={"uppercase text-center mr-[5px]"}>DAYS</p>
+            </div>
+            <div className={"text-white flex items-end mr-5"}>
+                <p className={"text-timer-num uppercase text-center mr-[5px]"}>{timeRemaining.hours}</p>
+                <p className={"uppercase text-center mr-[5px]"}>Hours</p>
+            </div>
+            <div className={"text-white flex items-end mr-5"}>
+                <p className={"text-timer-num uppercase text-center mr-[5px]"}>{timeRemaining.minutes}</p>
+                <p className={"uppercase text-center mr-[5px]"}>minutes</p>
+            </div>
+            <div className={"text-white flex items-end mr-5"}>
+                <p className={"text-timer-num uppercase text-center mr-[5px]"}>{timeRemaining.seconds}</p>
+                <p className={"uppercase text-center mr-[5px]"}>seconds</p>
+            </div>
+        </div>
+    );
+};
+
+export default Timer;
