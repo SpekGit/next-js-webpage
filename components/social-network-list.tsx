288b4044b148f16cbf970588d3f6ee56f74d66f1
Haller125 <connect@spektal.com>
2024-03-28 15:06:55 +0500

    added social network list for header

---
+++ components/social-network-list.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {InstagramIcon, LinkedinIcon, TelegramIcon, TwitterIcon, YoutubeIcon} from "@/components/bricks/icons";
+import Link from "next/link";
+
+let space:string = "20";
+const SocialNetworkList = () => {
+    return (
+        <div>
+            <div className={"flex"}>
+                <Link href={"#"} className={"mr-5"}>
+                    <LinkedinIcon/>
+                </Link>
+                <Link href={"#"} className={"mr-5"}>
+                    <InstagramIcon/>
+                </Link>
+                <Link href={"#"} className={"mr-5"}>
+                    <YoutubeIcon/>
+                </Link>
+                <Link href={"#"}  className={"mr-5"}>
+                    <TwitterIcon/>
+                </Link>
+                <Link href={"#"} className={"mr-5"}>
+                    <TelegramIcon/>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default SocialNetworkList;
