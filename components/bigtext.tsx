abbc1f8025335723baea0de260b4f89fcb376dcc
Haller125 <connect@spektal.com>
2024-03-29 15:37:12 +0500

    refactor

--- components/bigtext.tsx
+++ components/bigtext.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import H1Text from "@/components/bricks/h1-text";
 
 const Bigtext = () => {
     return (
--- components/social-network-list.tsx
+++ components/social-network-list.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {InstagramIcon, LinkedinIcon, TelegramIcon, TwitterIcon, YoutubeIcon} from "@/components/bricks/icons";
 import Link from "next/link";
 
+let space:string = "20";
 const SocialNetworkList = () => {
     return (
         <div>
