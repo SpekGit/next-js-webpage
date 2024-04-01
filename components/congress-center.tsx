223fd95ae8d0f6423379670adae01fefb2c1d713
Haller125 <connect@spektal.com>
2024-04-01 15:29:40 +0500

    add congress component

---
+++ components/congress-center.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CongressCenter = () => {
+    return (
+        <div>
+            <h2>Congress Center</h2>
+            <div>
+                <div>Map</div>
+                <img alt={"photo"}/>
+            </div>
+        </div>
+    );
+};
+
+export default CongressCenter;
