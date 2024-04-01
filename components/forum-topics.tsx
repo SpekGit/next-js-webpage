c5f00d487dbaec52ab03cde35656d50ac1a95d1a
Haller125 <connect@spektal.com>
2024-04-01 15:29:11 +0500

    add news and forum topics

---
+++ components/forum-topics.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ForumTopics = () => {
+    return (
+        <div>
+            <div>
+                <h2>Forum topics</h2>
+                <p>This year, AIF 2024 aims to build upon the successes of its inaugural edition in 2023, anticipating the
+                    participation of over 5000 delegates from more than 50 countries. AIF 2024 is poised to once again
+                    re-energize the global community, offering fresh perspectives and collaborative solutions to some of
+                    the most critical challenges facing humanity today.</p>
+            </div>
+            <div>
+                <div>Card1</div>
+                <div>Card2</div>
+                <div>Card3</div>
+                <div>Card4</div>
+            </div>
+        </div>
+    );
+};
+
+export default ForumTopics;
---
+++ components/news.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from "next/link";
+
+const News = () => {
+    return (
+        <div>
+            <div>
+                <h2>News</h2>
+                <a href={"#"}>All news</a>
+            </div>
+        </div>
+    );
+};
+
+export default News;
